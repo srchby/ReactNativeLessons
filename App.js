5e9893e21cf221ff003ab2db0c8425cdb19f3d96
searchby <raidentsuji@gmail.com>
2024-01-21 21:32:46 -0300

    created react native with expo project

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.Text}>React Native Lessons</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 'auto',
+    padding: 'auto',
   },
+  Text: {
+    fontFamily: 'Helvetica',
+  }
 });
